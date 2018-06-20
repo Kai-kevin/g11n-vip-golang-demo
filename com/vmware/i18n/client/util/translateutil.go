5439f865da7b0f73db5fc9ed19741ebc728d75cc
ruankai <kai.ruan@dilatoit.com>
2018-06-20 18:04:39 +0800

    fmt files

--- com/vmware/i18n/client/util/translateutil.go
+++ com/vmware/i18n/client/util/translateutil.go
@@ -1,11 +1,11 @@
 package util
 
 import (
-	"strings"
+	"errors"
 	"strconv"
+	"strings"
 	"vipgoclient/com/vmware/i18n/client/conf"
 	"vipgoclient/com/vmware/i18n/client/dao"
-	"errors"
 )
 
 var enableCache = conf.GetVipConfigInstance().EnableCache
@@ -13,41 +13,41 @@ var productID = conf.GetVipConfigInstance().ProductId
 var version = conf.GetVipConfigInstance().Version
 
 //get the translated value by key
-func getTranslationByKey(key, locale, component string) (string,error){
+func getTranslationByKey(key, locale, component string) (string, error) {
 
-	if(enableCache == "false"){
-		respEvent := dao.GetTranslationByKey(key ,locale, component)
+	if enableCache == "false" {
+		respEvent := dao.GetTranslationByKey(key, locale, component)
 
-		if(respEvent.Response.Code == 200){
+		if respEvent.Response.Code == 200 {
 
-			return respEvent.Data.Translation,nil
-		}else{
-			return "",errors.New("could't find the key:" + key + "value")
+			return respEvent.Data.Translation, nil
+		} else {
+			return "", errors.New("could't find the key:" + key + "value")
 		}
 
-	}else{
+	} else {
 		cacheDTO := CacheDTO{
-			Locale: locale,
-			Component:component,
+			Locale:    locale,
+			Component: component,
 			ProductID: productID,
-			Version:version,
+			Version:   version,
 		}
 
 		cacheMap := *GetCacheMap()
 
-		if cacheMap[cacheDTO][key] != ""{
-			return cacheMap[cacheDTO][key],nil
-		}else{
-			respEvent := dao.GetTranslationByKey(key ,locale, component)
+		if cacheMap[cacheDTO][key] != "" {
+			return cacheMap[cacheDTO][key], nil
+		} else {
+			respEvent := dao.GetTranslationByKey(key, locale, component)
 
-			if(respEvent.Response.Code == 200){
+			if respEvent.Response.Code == 200 {
 
 				//update cache
-				defer GetTranslationCacheManagerInstance().UpdateCacheByComponent(cacheDTO,map[string]string{key:respEvent.Data.Translation})
+				//defer GetTranslationCacheManagerInstance().UpdateCacheByComponent(cacheDTO, map[string]string{key: respEvent.Data.Translation})
 
-				return respEvent.Data.Translation,nil
-			}else{
-				return "",errors.New("could't find the key:" + key + "value")
+				return respEvent.Data.Translation, nil
+			} else {
+				return "", errors.New("could't find the key:" + key + "value")
 			}
 		}
 
@@ -55,18 +55,18 @@ func getTranslationByKey(key, locale, component string) (string,error){
 }
 
 //get the translated value by key and values
-func (*translationCacheManager) GetTranslationByKeyWithParams(key, locale, component string,params ...interface{}) (string,error){
+func (*translationCacheManager) GetTranslationByKeyWithParams(key, locale, component string, params ...interface{}) (string, error) {
 
-	value,err := getTranslationByKey(key,locale,component)
+	value, err := getTranslationByKey(key, locale, component)
 
 	if err != nil {
-		return value,err
+		return value, err
 	}
 
-	for index,param := range params{
-		value = strings.Replace(value,"{"+strconv.Itoa(index)+"}",param.(string),-1)
+	for index, param := range params {
+		value = strings.Replace(value, "{"+strconv.Itoa(index)+"}", param.(string), -1)
 	}
 
-	return value,nil
+	return value, nil
 
 }
